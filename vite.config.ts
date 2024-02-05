0708a37926b2e1e56520f4f443ba5d1fb72e43cd
Juan <juan.riglos@hotmail.com>
2024-02-05 08:50:58 -0300

    corrigiendo url para deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/flagApp/'
+  base: 'https://JuanPabloRiglos.github.io/flagApp/'
 })
